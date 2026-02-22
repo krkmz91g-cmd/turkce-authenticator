170f882824daa4a89b1ad6c39bb61671784f8287
krkmz91g-cmd <krkmz91g@gmail.com>
2026-02-23 02:07:02 +0300

    Create Tailwind CSS configuration file

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+/** Tailwind CSS Configuration File **/
+
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
