84a17ff395b08082d3f8bfd866032aa580c4e00e
krkmz91g-cmd <krkmz91g@gmail.com>
2026-02-23 02:06:54 +0300

    Create next.config.js with Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Add your Next.js config options here
+};
+
+module.exports = nextConfig;
